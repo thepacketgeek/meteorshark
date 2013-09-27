ca5a796d2957a24f82a284b10698462538a5300f
Mat Wood <mat@thepacketgeek.com>
2013-09-26 21:20:45 -0700

    Adding jQuery and Bootstrap packages.  Migrating HTML & CSS from cloudfish project.  Making some meteor specific CSS fixes.

--- meteorshark.js
+++ meteorshark.js
@@ -1,15 +1,15 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to meteorshark.";
-  };
+  // Template.hello.greeting = function () {
+  //   return "Welcome to meteorshark.";
+  // };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+  // Template.hello.events({
+  //   'click input' : function () {
+  //     // template data, if any, is available in 'this'
+  //     if (typeof console !== 'undefined')
+  //       console.log("You pressed the button");
+  //   }
+  // });
 }
 
 if (Meteor.isServer) {
