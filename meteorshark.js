1d4464ee76eb2bc5460449c70f674743dd271912
Mat Wood <mat@thepacketgeek.com>
2013-09-27 18:22:06 -0700

    Adding first iteration of login screen using built in UI for testing Packet collection per user

--- meteorshark.js
+++ meteorshark.js
@@ -12,28 +12,6 @@ if (Meteor.isClient) {
   Accounts.ui.config({
     passwordSignupFields: 'USERNAME_ONLY'
   });
-  Template.login.events({
-
-    'submit #login-form' : function(e, t){
-      e.preventDefault();
-      // retrieve the input field values
-      var username = t.find('#login-username').value
-        , password = t.find('#login-password').value;
-
-        // Trim and validate your fields here.... 
-
-        // If validation passes, supply the appropriate fields to the
-        // Meteor.loginWithPassword() function.
-        Meteor.loginWithPassword(username, password, function(err){
-        if (err) {
-          $('#loginError').html('Incorrect Login.');
-        } else {
-          
-        }
-      });
-         return false; 
-      }
-  });
 
   Template.buttons.events({
     'click #clear': function () {
