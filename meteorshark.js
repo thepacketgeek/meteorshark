784922473b9b5bfaa071be087cd8e1bf18e934a2
Mat Wood <mat@thepacketgeek.com>
2013-09-28 16:07:05 -0700

    Adding collection-api server side functions

--- meteorshark.js
+++ meteorshark.js
@@ -135,6 +135,37 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+  Meteor.startup(function () {
+
+    // All values listed below are default
+    collectionApi = new CollectionAPI({
+      authToken: undefined,              // Require this string to be passed in on each request
+      apiPath: 'api',                    // API path prefix
+      standAlone: false,                 // Run as a stand-alone HTTP(S) server
+      sslEnabled: false,                 // Disable/Enable SSL (stand-alone only)
+      listenPort: 3005,                  // Port to listen to (stand-alone only)
+      listenHost: undefined,             // Host to bind to (stand-alone only)
+      privateKeyFile: 'privatekey.pem',  // SSL private key file (only used if SSL is enabled)
+      certificateFile: 'certificate.pem' // SSL certificate key file (only used if SSL is enabled)
+    });
+
+    // Add the collection Players to the API "/players" path
+    collectionApi.addCollection(Packets, 'packets', {
+      // All values listed below are default
+      authToken: undefined,                   // Require this string to be passed in on each request
+      methods: ['POST'],  // Allow creating, reading, updating, and deleting
+      before: {  // This methods, if defined, will be called before the POST/GET/PUT/DELETE actions are performed on the collection. If the function returns false the action will be canceled, if you return true the action will take place.
+        POST: undefined,  // function(obj) {return true/false;},
+        GET: undefined,  // function(collectionID, objs) {return true/false;},
+        PUT: undefined,  //function(collectionID, obj, newValues) {return true/false;},
+        DELETE: undefined  //function(collectionID, obj) {return true/false;}
+      }
+    });
+
+    // Starts the API server
+    collectionApi.start();
+  });
+
   Meteor.publish("packets", function(id){
     return Packets.find({"owner": id});
   });
