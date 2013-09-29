e130474e43a12b92cbaf72a31b216543499afcfd
Mat Wood <mat@thepacketgeek.com>
2013-09-29 15:00:28 -0700

    Making a better dummy packet generator with actual timestamp values and random IP/MAC,srcPorts for more unique dummy packets.

--- postPacket.py
+++ postPacket.py
@@ -1,8 +1,34 @@
 import requests
 import json
+import random
+import time
+
+def randOct(end = 254):
+	return str(random.randint(1, end))
+
+## Used for random size or TTL
+def randTS(max = 256):
+	return str(random.randint(1, max/8) * 8)
+	
+def randHex(max = 65535):
+	return str(hex(random.randint(1, max)))[2:]
+	
 
 url = 'http://localhost:3000/api/packets'
-payload = {'owner': 'ZNss5F8svsW3wDWTX',"timestamp": "2.727","srcIP": "74.125.68.3","dstIP": "100.100.100.100","L7protocol": "HTTP","size": "243","ttl": "124","srcMAC": "1111.2222.3333","dstMAC": "4444.5555.6666","L4protocol": "TCP","srcPort": "12343","dstPort": "80","payload": "GET www.github.com/accounts"}
+payload = {'owner': 'ZNss5F8svsW3wDWTX',\
+	"timestamp": time.strftime("%m/%d/%y %I:%M:%S %p", time.gmtime()),\
+	"srcIP": (randOct() + "." + randOct() + "." + randOct() + "." + randOct()),\
+	"dstIP": (randOct() + "." + randOct() + "." + randOct() + "." + randOct()),\
+	"L7protocol": "HTTP",\
+	"size": randTS(1500),\
+	"ttl": randTS(),\
+	"srcMAC": (randHex() + "." + randHex() + "." + randHex()),\
+	"dstMAC": (randHex() + "." + randHex() + "." + randHex()),\
+	"L4protocol": "TCP",\
+	"srcPort": randTS(65000),\
+	"dstPort": "80",\
+	"payload": "GET www.github.com/accounts"}
+
 headers = {'content-type': 'application/json'}
 
 r = requests.post(url, data=json.dumps(payload), headers=headers)
